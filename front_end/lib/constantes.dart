c643101330cf37a72a76c673e1e84f40c50e6a82
Ahmed Krid <ahmed.krid68@gmail.com>
2025-03-21 23:17:03 +0100

    Add constantes.dart for color definitions and update widget_test.dart

---
+++ front_end/lib/constantes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+ //beleehi hethom awnouni fihom bech el kolha testamelhom
+ //colors 
+ const background_color = Color.fromARGB(255, 255, 255, 255);
+  const primary_color = Color.fromARGB(255, 0, 0, 0);
+
+//hetha el file te3 les contantes eli kolna nestamlouhem kima les bouton ou colors  bech kolna fard manthar
