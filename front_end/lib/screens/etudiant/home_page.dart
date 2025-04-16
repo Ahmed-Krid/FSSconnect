ecd0df55b9ff84b49a09c229ad9ad7f8754d6c6b
Ahmed Krid <ahmed.krid68@gmail.com>
2025-04-17 00:54:31 +0100

    bottom navigation bar

--- front_end/lib/screens/etudiant/home_page.dart
+++ front_end/lib/screens/etudiant/home_page.dart
@@ -10,7 +10,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _selectedIndex = 2; 
+  int _selectedIndex = 2;
   void _onItemSelected(int index) {
     setState(() {
       _selectedIndex = index;
